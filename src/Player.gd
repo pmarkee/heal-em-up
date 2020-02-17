ca4954d6f601138c82fbad7291a4a6ed390629d3
Peter Marki <marpeter@protonmail.com>
2020-02-17 16:43:50 +0100

    Add Player scene (kind of a dummy for now)

---
+++ src/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var velocity: = Vector2.ZERO
+var can_shoot: = false
+
+func shoot():
+    print("Shoot")
+
+func _physics_process(delta: float) -> void:
+    if Input.is_action_pressed("up"):
+        velocity.y = -300
+    elif Input.is_action_pressed("down"):
+        velocity.y = 300
+    else:
+        velocity.y = 0
+    
+    if Input.is_action_just_pressed("shoot") and can_shoot:
+        shoot()
+        can_shoot = false
+    
+    move_and_slide(velocity)
+
+
+func _on_ShootTimer_timeout() -> void:
+    can_shoot = true
