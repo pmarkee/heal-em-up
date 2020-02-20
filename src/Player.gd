2238212104609149d1e58ab25fbe06d42a8119ad
Péter Márki <marpeter@protonmail.com>
2020-02-20 18:06:28 +0100

    Create basic scenes and assets

--- src/Player.gd
+++ src/Player.gd
@@ -4,22 +4,23 @@ var velocity: = Vector2.ZERO
 var can_shoot: = false
 
 func shoot():
-    print("Shoot")
+	print("Shoot")
 
 func _physics_process(delta: float) -> void:
-    if Input.is_action_pressed("up"):
-        velocity.y = -300
-    elif Input.is_action_pressed("down"):
-        velocity.y = 300
-    else:
-        velocity.y = 0
-    
-    if Input.is_action_just_pressed("shoot") and can_shoot:
-        shoot()
-        can_shoot = false
-    
-    move_and_slide(velocity)
+	if Input.is_action_pressed("up"):
+		velocity.y = -300
+	elif Input.is_action_pressed("down"):
+		velocity.y = 300
+	else:
+		velocity.y = 0
+	
+	if Input.is_action_just_pressed("shoot") and can_shoot:
+		shoot()
+		can_shoot = false
+	
+	move_and_slide(velocity)
 
 
 func _on_ShootTimer_timeout() -> void:
-    can_shoot = true
+	can_shoot = true
+
---
+++ src/Rat.gd
@@ -0,0 +1,6 @@
+extends KinematicBody2D
+
+var velocity = Vector2.LEFT * 50
+
+func _physics_process(delta):
+	move_and_slide(velocity)
